4ba339dbba89d048943d79fe2d21794dc493a150
Christopher Bignamini <christopher.bignamini@gmail.com>
2016-12-09 18:38:39 +0100

    Basic UI implemented. Logic implementation ongoing

--- app/src/main/java/com/commutingcoder/firebaserealtimedatabasetest/MainActivity.java
+++ app/src/main/java/com/commutingcoder/firebaserealtimedatabasetest/MainActivity.java
@@ -2,7 +2,13 @@ package com.commutingcoder.firebaserealtimedatabasetest;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -17,6 +23,12 @@ public class MainActivity extends AppCompatActivity {
     private Position mOtherPosition;
     private Boolean mMyAudioStatus;
     private Boolean mOtherAudioStatus;
+    private EditText mMyLatitudeEditText;
+    private EditText mMyLongitudeEditText;
+    private Button mUpdateMyPositionButton;
+    private TextView mOtherLatitudeEditText;
+    private TextView mOtherLongitudeEditText;
+    private Button mUpdateOtherPositionButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,28 +38,109 @@ public class MainActivity extends AppCompatActivity {
         mMyAudioStatus = new Boolean(true);
         mOtherAudioStatus = new Boolean(false);
         mMyPosition = new Position(23.2,46.03);
-        mOtherPosition = new Position(1.9,1.7);
+        mOtherPosition = new Position(0.0,0.0);
+
+        // Wire UI stuff
+        mMyLatitudeEditText = (EditText) findViewById(R.id.my_latitude_text);
+        mMyLongitudeEditText = (EditText) findViewById(R.id.my_longitude_text);
+        mUpdateMyPositionButton = (Button) findViewById(R.id.update_my_position_button);
+        mOtherLatitudeEditText = (TextView) findViewById(R.id.other_latitude_text);
+        mOtherLongitudeEditText = (TextView) findViewById(R.id.other_longitude_text);
+        mUpdateOtherPositionButton = (Button) findViewById(R.id.update_other_position_button);
 
         // Setup reference to rt database
         FirebaseDatabase database = FirebaseDatabase.getInstance();
         DatabaseReference trackingReference = database.getReference("tracking_c1_c2");
         DatabaseReference myAudioStatusReference = trackingReference.child("audio_status").child("my_audio");
         DatabaseReference otherAudioStatusReference = trackingReference.child("audio_status").child("other_audio");
-        DatabaseReference myPositionReference = trackingReference.child("positions").child("my_position");
+        final DatabaseReference myPositionReference = trackingReference.child("positions").child("my_position");
         DatabaseReference otherPositionReference = trackingReference.child("positions").child("other_position");
+
+        // Initialize UI stuff
+        mMyLatitudeEditText.setText(String.valueOf(mMyPosition.getLatitude()));
+        mMyLatitudeEditText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                // TODO: check if better procedure exist
+                try {
+                    Double d = Double.parseDouble(s.toString());
+                    mMyPosition.setLatitude(Double.valueOf(s.toString()));
+                } catch (NumberFormatException ex) {
+                    // Do something smart here...
+                    mMyPosition.setLatitude(0.0);
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+        // TODO: check if better procedure exist
+        mMyLatitudeEditText.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View v, boolean hasFocus) {
+                if (hasFocus == false) {
+                    mMyLatitudeEditText.setText(String.valueOf(mMyPosition.getLatitude()));
+                }
+            }
+        });
+
+        mMyLongitudeEditText.setText(String.valueOf(mMyPosition.getLongitude()));
+        mMyLongitudeEditText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                mMyPosition.setLongitude(Double.valueOf(s.toString()));
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+        mOtherLatitudeEditText.setText(String.valueOf(mOtherPosition.getLatitude()));
+        mOtherLongitudeEditText.setText(String.valueOf(mOtherPosition.getLongitude()));
+        mUpdateMyPositionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                // Set value for my position
+                // TODO: have a look to custom java object usage
+                Log.d(TAG, "My position is: " + mMyPosition.getLatitude() + " " + mMyPosition.getLongitude());
+                myPositionReference.child("latitude").setValue(mMyPosition.getLatitude());
+                myPositionReference.child("longitude").setValue(mMyPosition.getLongitude());
+
+            }
+        });
+        mUpdateOtherPositionButton.setEnabled(false);// TODO: can we change color here?
+        mUpdateOtherPositionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mOtherLatitudeEditText.setText(String.valueOf(mOtherPosition.getLatitude()));
+                mOtherLongitudeEditText.setText(String.valueOf(mOtherPosition.getLongitude()));
+                mUpdateOtherPositionButton.setEnabled(false);
+            }
+        });
+
 
         // Set value for my audio status
         myAudioStatusReference.setValue(mMyAudioStatus);
 
-        // Set value for my position
-        // TODO: have a look to custom java object usage
-        myPositionReference.child("latitude").setValue(mMyPosition.getLatitude());
-        myPositionReference.child("longitude").setValue(mMyPosition.getLongitude());
 
-        // TODO: debug only
-        otherAudioStatusReference.setValue(mOtherAudioStatus);
-        otherPositionReference.child("latitude").setValue(mOtherPosition.getLatitude());
-        otherPositionReference.child("longitude").setValue(mOtherPosition.getLongitude());
+//        // TODO: debug only
+//        otherAudioStatusReference.setValue(mOtherAudioStatus);
+//        otherPositionReference.child("latitude").setValue(mOtherPosition.getLatitude());
+//        otherPositionReference.child("longitude").setValue(mOtherPosition.getLongitude());
 
         // Set other audio status value change event listener
         otherAudioStatusReference.addValueEventListener(new ValueEventListener() {
@@ -74,6 +167,7 @@ public class MainActivity extends AppCompatActivity {
                 // whenever data at this location is updated.
                 mOtherPosition.setLatitude((double) (dataSnapshot.child("latitude").getValue()));
                 mOtherPosition.setLongitude((double) (dataSnapshot.child("longitude").getValue()));
+                mUpdateOtherPositionButton.setEnabled(true);
                 Log.d(TAG, "Other position is: " + mOtherPosition.getLatitude() + " " + mOtherPosition.getLongitude());
             }
 
